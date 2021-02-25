763df320edd9cf2d54cecd50c47a114f2269ddce
Fletcher Rippon <fletcherrippon@gmail.com>
2021-02-25 13:51:38 +1100

    first commit

---
+++ resources/views/myPage.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>My Page</title>
+  <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+  <div>
+    <h1>Template with {{ strtoupper('blade') }}</h1>
+    <h2>Make a for <code>loop</code></h2>
+    <ul>
+      @for ($i = 0; $i <= 2; $i++)
+        <li>Number {{ $i }}</li>
+      @endfor
+    </ul>
+    <h3>Data parsed from router using <code>foreach</code></h3>
+    <ul>
+      @foreach ($user as $data)
+        <li>{{ $data }}</li>
+      @endforeach
+    </ul>
+  </div>
+</body>
+</html>
---
+++ routes/api.php
@@ -0,0 +1,95 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Say hello world -> returns as json
+Route::get('/', function () {
+    return ['message' => 'Hello World'];
+});
+
+// Sends random number
+Route::get('/give-random-number', function () {
+    return rand();
+});
+
+// Using params returns a post
+Route::get('/post/{id}', function ($id) {
+    switch ($id) {
+        case 0:
+            return 'post 0';
+            break;
+        case 1: 
+            return 'post 1';
+            break;
+        default:
+            return 'no post with id of '.$id;
+            break;
+    }
+});
+
+// Grouped routes 
+Route::prefix('/profile')->group(function () {
+    // All profile data
+    Route::get('/', function () {
+        $first_name = 'Fletcher';
+        $last_name = 'Rippon';
+        $bio = 'Things about me';
+
+        return [
+            'firstName' => $first_name,
+            'lastName' => $last_name,
+            'fullName' => $first_name.' '.$last_name,
+            'bio' => $bio,
+            'bioLength' => strlen($bio)
+        ];
+    });
+
+    // Optional pram 
+    // profile/name/{sub_name: first | last (optional)}
+    Route::get('/name/{sub_name?}', function ($sub_name = null) {
+        $first_name = 'Fletcher';
+        $last_name = 'Rippon';
+
+        if (is_null($sub_name)) {
+            return $first_name.' '.$last_name;
+        }
+
+        switch($sub_name) {
+            case('first'):
+                return $first_name;
+                break;
+            case('last'): 
+                return $last_name;
+                break;
+            default:
+                return 'Unknown param';
+                break;
+        }
+    });
+
+    // Return bio
+    Route::get('/bio', function () {
+        $bio = 'Things about me';
+
+        return [
+            'bio' => $bio,
+            'bioLength' => strlen($bio)
+        ];
+    });
+});
